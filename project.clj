51e90f325a3db8aac4065fd615d3ed6c2e4a8b4c
Adam Harper <github@adam-harper.com>
2017-03-21 15:35:08 +0000

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject qrul "0.1.2-SNAPSHOT"
+(defproject qrul "0.2.0"
   :description "Quick Report Usage Logger"
   :url "https://github.com/lymingtonprecision/qrul"
   :license {:name "Eclipse Public License"
